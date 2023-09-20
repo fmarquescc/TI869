5bc3645b527f1abaa86552f64d6b72b201249031
fmarquescc <filipemarqcc2023@gmail.com>
2023-09-20 14:26:01 +0100

    macros de calculo de area e perimetros implementados

--- Trigonometria/main.c
+++ Trigonometria/main.c
@@ -1,6 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define PI 3.1415
+
+#define CALC_AREA_CIRCLE(r) (PI * r * r)
+#define CALC_PERI_CIRCLE(r) (PI * 2 * r)
+
+#define CALC_AREA_SQUARE(l) (l * l)
+#define CALC_PERI_SQUARE(l) (l * 4)
+
+#define CALC_AREA_RECTANGLE(b, a) (b * a)
+#define CALC_PERI_RECTANGLE(b, a) ((b + a) * 2)
+
+#define CALC_AREA_TRIANGLE(b, a) ((b * a) / 2)
+#define CALC_PERI_TRIANGLE(b, c1, c2) (b + c1 + c2)
+
 int main()
 {
     return 0;
